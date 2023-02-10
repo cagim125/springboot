56d348d2423ac3f83c223b345960072a29b6b67a
cagim125 <cagim30@gmail.com>
2023-02-10 15:53:21 +0900

    third commit

---
+++ src/main/java/com/omgstudy/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.omgstudy.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/test/java/com/omgstudy/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.omgstudy.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.*;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "test_title";
+        String content = "test_content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("cagim30@gmail.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
