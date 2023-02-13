6ecd9aef1ccc4ed709c3769d91f3a54c8799a02c
ohmingyu <cagim30@gmail.com>
2023-02-13 17:56:14 +0900

    0213 commit mustache add

---
+++ src/test/java/com/omgstudy/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,11 @@
+package com.omgstudy.book.springboot.web;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runner.Runner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Runner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+}
