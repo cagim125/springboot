2bad6b77e7e5e24a1d3279e845efb409ee5913c5
cagim125 <cagim30@gmail.com>
2023-02-13 23:23:01 +0900

    게시글 작성 form

--- src/test/java/com/omgstudy/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/omgstudy/book/springboot/web/IndexControllerTest.java
@@ -1,11 +1,29 @@
 package com.omgstudy.book.springboot.web;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.junit.runner.Runner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(Runner.class)
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부토로 시작하는 웹 서비스");
+    }
 }
